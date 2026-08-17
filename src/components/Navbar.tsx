5a8a9c8682758a6c97540761c817d0760c7c9510
Zahra Khansari <zarakhansari@gmail.com>
2026-08-17 17:16:09 +0200

    feat: integrate Tailwind CSS and React Router with updated Navbar layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,20 +2,24 @@ import { Link } from "react-router-dom";
 
 
 function Navbar() {
-    return (<nav className='flex items-center justify-between px-6 py-6 md:px-12' >
-        {/* name section */}
-        <Link to="/">
-            <p className="font-bold text-2xl">Zahra Khansari </p>
-        </Link>
-        {/* menu items */}
-        <div>
-            <Link to="/"
-                className="text-gray-700 transition hover:text-black">Home</Link>
-            <Link to="/about"
-                className="text-gray-700 transition hover:text-black">About</Link>
-            <Link to="/portfolio"
-                className="text-gray-700 transition hover:text-black">Projects</Link>
-            <Link to="/contact" className="text-gray-700 transition hover:text-black">Contact</Link>
+    return (<nav className='mx-auto mt-6 max-w-6xl rounded-xl border border-gray-200 bg-white px-8 py-5 shadow-sm' >
+        <div className="mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
+
+
+            {/* name section */}
+            <Link to="/">
+                <p className="text-xl font-bold text-gray-900 transition hover:text-blue-600">Zahra Khansari </p>
+            </Link>
+            {/* menu items */}
+            <div className="left ">
+                <Link to="/"
+                    className="text-red-700 transition hover:text-black">Home</Link>
+                <Link to="/about"
+                    className="text-gray-700 transition hover:text-black">About</Link>
+                <Link to="/portfolio"
+                    className="text-gray-700 transition hover:text-black">Projects</Link>
+                <Link to="/contact" className="text-gray-700 transition hover:text-black">Contact</Link>
+            </div>
         </div>
 
     </nav>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Navbar from './components/Navbar'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
